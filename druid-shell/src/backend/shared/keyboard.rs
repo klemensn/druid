d815bc1e944742c9b1be2a61f1311df505121fce
Klemens Nanni <kn@openbsd.org>
2021-09-28 09:31:30 +0200

    Support OpenBSD
    Just like Linux, OpenBSD is sufficiently supported using GTK+3.
    No platform specific changes required to successfully run the examples
    and real-world applications like Psst (spotify GUI client).

--- druid-shell/src/backend/shared/keyboard.rs
+++ druid-shell/src/backend/shared/keyboard.rs
@@ -17,7 +17,7 @@
 #[allow(unused)]
 use keyboard_types::{Code, Location};
 
-#[cfg(any(all(feature = "x11", target_os = "linux"), target_os = "macos"))]
+#[cfg(any(all(feature = "x11", any(target_os = "linux", target_os = "openbsd")), target_os = "macos"))]
 /// Map key code to location.
 ///
 /// The logic for this is adapted from InitKeyEvent in TextInputHandler (in the Mozilla
@@ -51,7 +51,7 @@ pub fn code_to_location(code: Code) -> Location {
     }
 }
 
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "linux", target_os = "openbsd"))]
 /// Map hardware keycode to code.
 ///
 /// In theory, the hardware keycode is device dependent, but in
